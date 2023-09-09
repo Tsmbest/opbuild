145e2b73d88036650994e628dce1ab958d204757
Tsmbest <35428264+Tsmbest@users.noreply.github.com>
2023-09-09 16:51:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,10 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
+echo 'src-git dockerman https://github.com/lisaac/luci-app-dockerman.git' >>feeds.conf.default
+git clone https://github.com/xkstudio/luci-app-pptp-server.git package/pptp
+echo 'src-git openvswitch git://github.com/pichuang/openvwrt.git' >>feeds.conf.default
+echo 'src-git adguardhome git://github.com/pichuang/openvwrt.git' >>feeds.conf.default
+echo 'src-git openclash git://github.com/pichuang/openvwrt.git' >>feeds.conf.default
